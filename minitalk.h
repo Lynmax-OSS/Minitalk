41782264d60eb045505b0941e7163bf24ec95d15
keith.teo2005@gmail.com <keith.teo2005@gmail.com>
2025-03-18 11:00:03 +0800

    implement a way for the server to recieve the string from client and write it(does not have unicode character support)

--- minitalk.h
+++ minitalk.h
@@ -3,11 +3,19 @@
 # include <unistd.h>
 # include <signal.h>
 # include <stdlib.h>
-#include <stdbool.h>
+# include <stdbool.h>
+# include <limits.h>
+
+enum
+{
+	READY,
+	BUSY,
+};
 
 void	Signal(int , void *, bool);
-int	ft_printf(const char *format, ...);
-void	ft_putstr_fd(char *s, int fd);
-int	ft_atoi(const char *str);
+void	Kill(pid_t pid, int signo);
+int		ft_printf(const char *format, ...);
+void	ft_putstr_fd(char *c, int fd);
+int		ft_atoi(const char *str);
 
 #endif
--- server.c
+++ server.c
@@ -3,7 +3,28 @@
 void handler(int signo, siginfo_t *info, void *more_info)
 {
 	(void)more_info;
-	write(STDOUT_FILENO, "Signal received\n", 16);
+	static char		c;
+	static int		bit;
+	static pid_t	client;
+
+	client = info->si_pid;
+	if (SIGUSR1 == signo)
+		c |= (0b10000000 >> bit);
+	else if (SIGUSR2 == signo)
+		c &= ~(0b10000000 >> bit);
+	++bit;
+	if (CHAR_BIT == bit)
+	{
+		bit = 0;
+		if ('\0' == c)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			Kill(client, SIGUSR2);
+			return ;
+		}
+		write(STDOUT_FILENO, &c, 1);
+	}
+	kill(client, SIGUSR1);
 }
 
 int main(void)
